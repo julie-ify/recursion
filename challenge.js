a638a669d48db334a155fd7ec8d4a3fdec0bcc07
Juliana Ifionu <julieifionu@gmail.com>
2021-09-28 08:42:39 +0100

    Solution

--- challenge.js
+++ challenge.js
@@ -1,6 +1,9 @@
 module.exports = function sum(number) {
   // write your code here
-}
+  const total = 0;
+  if (number < 1) return total;
+  return number + sum(number - 1);
+};
 
-console.log(sum(4))
-console.log(sum(10))
+// console.log(sum(4));
+// console.log(sum(10));
